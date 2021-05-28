61c83fdc89faeea33eb2aa327fb7136be9855e84
BushraKhalid01 <bsef18m018@pucit.edu.pk>
2021-05-28 17:17:16 +0500

    commit 4 TimePickerDialog added

--- MC_Reminder_ToDoList/app/src/main/java/org/haqnawaz/mc_reminder_todolist/AddReminder.java
+++ MC_Reminder_ToDoList/app/src/main/java/org/haqnawaz/mc_reminder_todolist/AddReminder.java
@@ -3,11 +3,13 @@ package org.haqnawaz.mc_reminder_todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
@@ -16,10 +18,17 @@ public class AddReminder extends AppCompatActivity {
     int day;
     int month;
     int year;
+    int currHour;
+    int currMin;
+    String amPm;
     Calendar calendar;
     TextView date1;
     TextView date2;
+    TextView time1;
+    TextView time2;
+    TimePickerDialog timePickerDialog;
     DatePickerDialog datePickerDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,8 @@ public class AddReminder extends AppCompatActivity {
         setContentView(R.layout.activity_add_reminder);
         date1 = findViewById(R.id.date_text);
         date2 = findViewById(R.id.set_date);
+        time1 = findViewById(R.id.time_text);
+        time2 = findViewById(R.id.set_time);
     }
 
     public void selectDate(View view) {
@@ -42,5 +53,25 @@ public class AddReminder extends AppCompatActivity {
             }
         },year,month,day);
         datePickerDialog.show();
+    }
+
+    public void selectTime(View view) {
+        calendar=Calendar.getInstance();
+        currHour=calendar.get(Calendar.HOUR_OF_DAY);
+        currMin=calendar.get(Calendar.MINUTE);
+
+        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                if(hourOfDay>=12){
+                    amPm="PM";
+                }
+                else{
+                    amPm="AM";
+                }
+                time2.setText(String.format("%02d:%02d",hourOfDay, minute)+" "+amPm);
+            }
+        },currHour,currMin,false);
+        timePickerDialog.show();
     }
 }
