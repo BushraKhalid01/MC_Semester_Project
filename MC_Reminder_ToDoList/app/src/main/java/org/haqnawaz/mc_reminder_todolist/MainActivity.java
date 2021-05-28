a8e577cb7288ac35a24e934685301d1f18284e9f
BushraKhalid01 <bsef18m018@pucit.edu.pk>
2021-05-28 14:23:07 +0500

    commit 1 mainActivity added

---
+++ MC_Reminder_ToDoList/app/src/main/java/org/haqnawaz/mc_reminder_todolist/MainActivity.java
@@ -0,0 +1,25 @@
+package org.haqnawaz.mc_reminder_todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.ContentUris;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
